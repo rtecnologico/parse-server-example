115fd16b163522fc35b67e20b48d9d353c48eee9
Rafael Souza <rcsouza23@gmail.com>
2016-10-30 00:30:22 -0300

    Update index.js

--- index.js
+++ index.js
@@ -45,6 +45,10 @@ app.get('/test', function(req, res) {
   res.sendFile(path.join(__dirname, '/public/test.html'));
 });
 
+app.get('/brocou', function(req, res) {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
 var port = process.env.PORT || 1337;
 var httpServer = require('http').createServer(app);
 httpServer.listen(port, function() {
